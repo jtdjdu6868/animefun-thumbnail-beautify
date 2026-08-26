d84d8c1a50071be22ecfed1acc23a2c9bdbeaa79
jtdjdu6868 <99466048+jtdjdu6868@users.noreply.github.com>
2026-08-26 19:31:49 +0800

    Use GM_addStyle insteadof manually add style tag

--- beautify.user.js
+++ beautify.user.js
@@ -3,32 +3,28 @@
 // @name:zh-TW   動畫瘋年齡確認美化
 // @namespace    http://github.com/jtdjdu6868
 // @version      0.1
-// @description  Beautify Animefun ncc confirm page
-// @description:zh-TW    美化動畫瘋年齡確認畫面，使集數縮圖可以顯示
+// @description  Beautify Animefun NCC confirm page
+// @description:zh-TW 美化動畫瘋年齡確認畫面，使集數縮圖可以顯示
 // @author       jtdjdu6868
 // @match        https://ani.gamer.com.tw/animeVideo.php*
 // @icon         https://ani.gamer.com.tw/favicon.ico
-// @grant        none
+// @grant        GM_addStyle
 // @license      MIT
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    $(`<style>
-        .R18
-        {
-            background: transparent;
-        }
-        .R18 .video-cover-ncc
-        {
-            top: auto;
-            bottom: 0;
-            transform: none;
-            background-color: #00000026
-        }
-        .R18 .video-cover-ncc .ncc-choosearea
-        {
-            justify-content: inherit;
-        }
-    </style>`).appendTo("body");
-})();
+GM_addStyle(`
+    div.R18 {
+        background: transparent;
+    }
+
+    .R18 div.video-cover-ncc {
+        top: auto;
+        bottom: 0;
+        transform: none;
+        background-color: #00000026;
+    }
+
+    .R18 .video-cover-ncc div.ncc-choosearea {
+        justify-content: inherit;
+    }
+`);
